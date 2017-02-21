563c05ddd33eb71a6b1c334a6c7be9b08cfe0f15
Mahtiah <mahtiah@gmail.com>
2017-02-21 13:25:13 +0100

    Ajout d'une page sous forme de FAQ avec tout le développement historique/social/économique et début de réponses à ses questions !

---
+++ scripts/quizz.js
@@ -0,0 +1,10 @@
+/*
+On établit un tableau de chaîne de caractères (les questions).
+On établit un tableau de booléen true pour oui false pour non.
+On génère les radio button et les listes à puces.
+Quand on clique sur le bouton Valider :
+--> On parcout les boutons checked.
+--> En fonction de leur indice si ils sont à leur bonne valeur (oui/non) on incrémente la note.
+--> On affiche la note d'une quelquonque manière.
+HAVE FUN
+*/
